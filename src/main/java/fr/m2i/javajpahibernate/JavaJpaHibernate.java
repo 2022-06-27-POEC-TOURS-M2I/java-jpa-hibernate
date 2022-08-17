cd61ae1fb5cc6138d18510dd511837fee5510bec
Tiity <cadet_a@etna-alternance.net>
2022-08-17 11:34:25 +0200

    Session helper

--- src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
+++ src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
@@ -1,14 +1,16 @@
 package fr.m2i.javajpahibernate;
 
+import fr.m2i.javajpahibernate.helper.SessionHelper;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class JavaJpaHibernate {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("site_commerce_pu");
-        EntityManager entityManager = emf.createEntityManager();
-        System.out.println("Hello world!");
+
+        EntityManager entityManager = SessionHelper.getEntityManager();
+
+        System.out.println("Nos traitements avec l'entity manager...");
+
+        entityManager.close();
     }
 }
