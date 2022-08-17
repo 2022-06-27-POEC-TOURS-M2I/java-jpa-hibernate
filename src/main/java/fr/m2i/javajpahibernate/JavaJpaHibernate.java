160d14274574089863d1dcbd4c038186a7c61505
Tiity <cadet_a@etna-alternance.net>
2022-08-17 16:05:44 +0200

    Role DAO v1

--- src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
+++ src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
@@ -1,6 +1,8 @@
 package fr.m2i.javajpahibernate;
 
+import fr.m2i.javajpahibernate.dao.RoleDAO;
 import fr.m2i.javajpahibernate.helper.SessionHelper;
+import fr.m2i.javajpahibernate.model.Role;
 import javax.persistence.EntityManager;
 
 public class JavaJpaHibernate {
@@ -8,8 +10,25 @@ public class JavaJpaHibernate {
     public static void main(String[] args) {
 
         EntityManager entityManager = SessionHelper.getEntityManager();
+        RoleDAO roleDao = new RoleDAO();
 
-        System.out.println("Nos traitements avec l'entity manager...");
+        // create
+        Role roleAdmin = new Role("ADMIN", "Le rôle Administrateur"); 
+        roleDao.create(roleAdmin);
+
+        // find
+        Role founded = roleDao.findById(1L);
+        System.out.println("Role CREATED : " + founded);
+        
+        // update
+        Role roleData = new Role();
+        roleData.setDescription("Le rôle Administrateur implique de grandes responsabilités");
+
+        roleDao.update(1L, roleData);
+
+        // find
+        Role updated = roleDao.findById(1L);
+        System.out.println("Role UPDATED : " + updated);
 
         entityManager.close();
     }
